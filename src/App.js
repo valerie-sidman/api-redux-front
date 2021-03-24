a3e01afe3607595ecc09a2cdded751adac167de0
Valerie Sidman <valerie.sidman@gmail.com>
2021-03-24 11:39:37 +0300

    added hashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Add from './components/Add';
 import List from './components/List';
 import Edit from './components/Edit';
